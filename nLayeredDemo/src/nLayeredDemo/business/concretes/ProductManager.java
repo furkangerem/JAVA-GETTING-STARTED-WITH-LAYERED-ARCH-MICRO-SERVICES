bee4cfd802df7096401614af987daa82429c7059
MFG <MFG@MSI>
2022-05-14 11:59:45 +0300

    Java, getting started wtih nLayered arch and micro services.

---
+++ nLayeredDemo/src/nLayeredDemo/business/concretes/ProductManager.java
@@ -0,0 +1,40 @@
+package nLayeredDemo.business.concretes;
+
+import java.util.List;
+
+import nLayeredDemo.business.abstracts.ProductService;
+import nLayeredDemo.core.LoggerService;
+import nLayeredDemo.dataAccess.abstracts.ProductDao;
+import nLayeredDemo.dataAccess.concretes.HibernateProductDao;
+import nLayeredDemo.entities.concretes.Product;
+
+public class ProductManager implements ProductService {
+	
+	private ProductDao productDao;
+	private LoggerService loggerService;
+
+	public ProductManager(ProductDao productDao, LoggerService loggerService) {
+		super();
+		this.productDao = productDao;
+		this.loggerService = loggerService;
+	}
+
+	@Override
+	public void add(Product product) {
+		
+		// Business Codes in Here.
+		if (product.getCategoryId() == 1) {
+			System.out.println("Product isn't allowed in this category.");
+			return;
+		}
+		
+		this.productDao.add(product);
+		this.loggerService.logToSystem("Product is added: " + product.getName());
+	}
+
+	@Override
+	public List<Product> getAll() {
+		return null;
+	}
+
+}
---
+++ nLayeredDemo/src/nLayeredDemo/jLogger/JLoggerManager.java
@@ -0,0 +1,9 @@
+package nLayeredDemo.jLogger;
+
+public class JLoggerManager {
+
+	public void log (String message) {
+		
+		System.out.println("Logged with third party app that is JLogger.");
+	}
+}
